df83d8f7484bdbd1683b542fea6d129539d7191e
aortizpalma <77061458+aortizpalma@users.noreply.github.com>
2021-12-04 07:31:12 +0200

    Day 2, part 1 task resolved

---
+++ day2/aoc_2021_day2_1.js
@@ -0,0 +1,102 @@
+const day2Input = ["forward 8", "down 9", "up 1", "forward 2", "down 6", "forward 6", "down 5", "down 4", "down 2", "forward 3", "forward 8", "down 6", "up 3", "up 3", "down 5", "up 8", "forward 8", "up 6", "forward 4", "down 8", "up 2", "up 6", "forward 7", "up 6", "down 6", "down 1", "forward 8", "up 7", "forward 9", "forward 5", "forward 8", "forward 5", "down 3", "forward 5", "up 1", "forward 6", "forward 3", "forward 3", "down 2", "forward 4", "down 3", "forward 6", "forward 2", "forward 1", "down 2", "forward 1", "down 1", "up 8", "down 4", "forward 3", "up 3", "down 5", "down 2", "forward 7", "forward 1", "down 9", "up 1", "down 1", "forward 2", "forward 1", "forward 1", "forward 7", "up 5", "forward 2", "down 6", "down 7", "down 3", "down 1", "forward 4", "forward 5", "down 3", "forward 4", "down 5", "forward 3", "forward 4", "forward 2", "up 8", "down 7", "up 3", "forward 2", "down 5", "forward 9", "forward 5", "up 1", "down 5", "down 6", "forward 2", "forward 5", "down 6", "down 1", "up 8", "up 9", "down 7", "down 5", "forward 2", "forward 7", "forward 7", "up 6", "down 4", "up 6", "forward 2", "down 4", "down 3", "down 4", "forward 2", "down 2", "down 1", "down 4", "up 4", "forward 7", "forward 6", "forward 2", "forward 5", "forward 1", "forward 7", "forward 2", "down 3", "forward 7", "forward 2", "up 4", "up 5", "down 1", "down 2", "down 3", "down 7", "up 8", "forward 4", "down 5", "down 4", "forward 3", "down 6", "forward 5", "up 5", "forward 6", "down 1", "down 7", "forward 1", "forward 6", "up 9", "forward 2", "down 1", "forward 5", "forward 2", "forward 6", "forward 8", "forward 6", "down 8", "forward 3", "down 9", "up 8", "forward 9", "down 7", "down 6", "down 1", "up 5", "down 6", "forward 7", "up 9", "forward 8", "forward 3", "forward 3", "forward 2", "down 6", "up 1", "up 5", "forward 7", "down 2", "down 5", "down 1", "down 4", "forward 8", "forward 2", "forward 8", "up 5", "forward 7", "up 7", "forward 6", "forward 9", "up 1", "down 4", "forward 8", "forward 4", "down 9", "up 6", "down 8", "up 9", "down 2", "down 2", "down 9", "up 3", "down 7", "down 2", "up 9", "forward 9", "down 5", "forward 2", "forward 1", "forward 2", "down 4", "forward 4", "down 2", "forward 8", "up 3", "forward 6", "up 7", "up 9", "forward 4", "up 9", "down 5", "forward 5", "up 6", "down 2", "down 4", "forward 1", "down 2", "forward 3", "down 7", "down 1", "down 5", "forward 5", "up 5", "forward 3", "down 5", "forward 7", "down 3", "down 2", "up 9", "down 4", "forward 4", "down 4", "forward 1", "up 9", "down 8", "forward 2", "down 7", "forward 8", "down 5", "up 2", "down 9", "down 2", "up 4", "up 2", "forward 8", "forward 3", "forward 4", "down 1", "down 3", "forward 3", "down 8", "forward 7", "down 7", "forward 5", "forward 5", "up 3", "down 6", "forward 1", "down 4", "forward 2", "down 4", "up 2", "down 3", "forward 4", "forward 6", "up 7", "down 3", "forward 7", "forward 1", "down 1", "up 3", "down 7", "up 3", "up 8", "up 7", "up 7", "forward 7", "down 2", "forward 6", "forward 7", "up 3", "down 3", "up 9", "down 6", "down 7", "down 7", "up 5", "forward 6", "forward 2", "up 9", "forward 2", "down 3", "down 3", "forward 5", "forward 3", "forward 6", "forward 6", "down 7", "forward 2", "down 3", "up 4", "forward 1", "forward 5", "down 5", "up 1", "down 9", "forward 3", "up 6", "forward 5", "down 4", "forward 9", "up 5", "forward 7", "up 7", "forward 9", "forward 2", "forward 6", "forward 3", "forward 3", "up 7", "down 1", "down 1", "forward 1", "down 8", "down 2", "forward 4", "up 7", "forward 7", "down 5", "down 7", "up 6", "down 4", "forward 4", "forward 4", "up 7", "down 9", "forward 6", "forward 1", "down 7", "forward 5", "forward 2", "forward 1", "up 7", "down 9", "forward 2", "down 7", "up 8", "forward 1", "forward 4", "up 4", "forward 1", "down 9", "down 5", "down 8", "forward 8", "forward 8", "down 8", "down 3", "down 5", "down 6", "up 9", "forward 1", "down 1", "forward 1", "forward 1", "down 6", "forward 3", "down 1", "forward 9", "forward 3", "up 7", "down 9", "forward 5", "down 4", "up 9", "down 2", "down 5", "down 6", "forward 1", "up 2", "forward 9", "up 7", "forward 2", "forward 8", "down 5", "down 6", "down 8", "forward 4", "down 4", "forward 9", "forward 3", "forward 6", "forward 9", "up 2", "down 8", "up 2", "up 6", "up 5", "down 5", "down 7", "forward 1", "up 6", "forward 9", "up 4", "forward 4", "up 2", "forward 3", "down 8", "down 1", "up 5", "forward 5", "down 3", "down 4", "down 1", "forward 3", "up 2", "forward 3", "down 1", "forward 8", "up 3", "forward 4", "down 3", "forward 6", "down 9", "down 7", "forward 3", "forward 5", "forward 1", "forward 3", "up 8", "forward 2", "forward 2", "forward 7", "up 9", "forward 4", "down 1", "down 6", "forward 8", "forward 5", "up 4", "up 5", "up 9", "forward 9", "up 8", "down 3", "up 5", "forward 1", "forward 5", "up 9", "forward 8", "forward 4", "forward 3", "forward 1", "forward 6", "down 2", "up 8", "down 7", "down 1", "down 5", "up 1", "forward 9", "down 2", "down 4", "forward 3", "forward 1", "down 7", "up 9", "up 7", "down 2", "down 4", "down 1", "forward 9", "up 4", "down 9", "down 9", "down 4", "down 9", "forward 1", "down 2", "forward 8", "up 4", "up 3", "forward 4", "down 5", "forward 8", "forward 2", "forward 4", "up 3", "up 2", "up 3", "forward 4", "forward 4", "forward 6", "down 7", "down 8", "up 3", "down 9", "down 5", "up 7", "up 1", "down 3", "down 4", "up 5", "up 9", "down 2", "up 8", "forward 9", "down 3", "forward 4", "down 5", "down 1", "forward 9", "forward 2", "up 3", "down 9", "down 8", "up 7", "forward 2", "down 9", "up 8", "up 2", "down 3", "forward 3", "forward 3", "forward 3", "forward 4", "down 7", "up 2", "forward 3", "forward 7", "forward 5", "forward 7", "down 6", "down 9", "down 6", "down 8", "up 4", "down 4", "down 2", "up 1", "down 3", "forward 7", "forward 5", "down 7", "forward 1", "up 7", "forward 9", "down 8", "down 1", "down 3", "down 1", "up 8", "forward 7", "up 5", "forward 6", "down 1", "down 8", "up 1", "up 9", "forward 7", "down 9", "forward 3", "forward 8", "forward 7", "down 3", "down 5", "up 3", "up 7", "up 3", "up 6", "up 8", "down 3", "forward 7", "down 7", "forward 9", "up 9", "down 1", "down 3", "down 8", "down 6", "forward 1", "up 6", "down 1", "up 4", "down 2", "forward 9", "up 8", "up 2", "down 6", "down 1", "forward 2", "forward 6", "forward 1", "forward 2", "up 6", "down 7", "down 8", "forward 9", "up 2", "down 6", "down 8", "down 5", "up 6", "forward 6", "down 5", "forward 4", "forward 2", "down 1", "down 4", "up 5", "down 5", "down 1", "down 9", "down 1", "down 1", "forward 1", "forward 5", "up 4", "down 5", "down 3", "down 2", "down 7", "forward 5", "forward 6", "forward 2", "up 9", "up 3", "forward 6", "down 4", "forward 8", "forward 4", "forward 9", "down 6", "down 9", "down 1", "forward 6", "down 8", "down 7", "forward 5", "down 4", "forward 5", "forward 5", "up 3", "down 1", "down 4", "forward 1", "forward 7", "down 5", "down 4", "forward 7", "forward 3", "forward 4", "down 4", "forward 8", "up 2", "up 6", "down 9", "up 1", "down 8", "up 2", "up 6", "forward 5", "forward 7", "up 4", "down 4", "down 8", "forward 5", "up 4", "down 6", "forward 9", "up 4", "forward 7", "up 7", "down 3", "down 2", "down 2", "down 6", "down 4", "forward 4", "down 3", "down 4", "down 4", "down 8", "up 2", "down 3", "forward 5", "forward 5", "up 5", "forward 1", "down 7", "forward 5", "forward 7", "forward 9", "forward 4", "down 1", "down 1", "up 5", "down 6", "down 8", "down 9", "down 9", "forward 7", "down 6", "forward 3", "down 7", "up 3", "forward 5", "forward 5", "up 6", "down 5", "down 8", "down 5", "down 1", "down 7", "forward 8", "up 9", "forward 2", "up 1", "down 9", "down 2", "forward 7", "up 1", "down 5", "forward 3", "up 6", "forward 9", "up 9", "down 3", "forward 2", "up 6", "down 6", "up 8", "down 3", "up 5", "forward 9", "up 3", "forward 4", "forward 6", "down 3", "forward 4", "down 5", "down 7", "down 7", "forward 9", "forward 6", "down 9", "up 1", "forward 2", "down 8", "down 7", "up 7", "forward 6", "up 5", "down 6", "forward 6", "down 3", "forward 2", "down 7", "forward 2", "down 3", "down 8", "forward 4", "up 8", "forward 7", "down 4", "forward 3", "down 3", "forward 6", "down 7", "down 7", "forward 8", "forward 9", "down 8", "down 4", "forward 5", "down 1", "forward 9", "down 7", "up 3", "down 6", "down 4", "down 9", "forward 3", "down 1", "forward 1", "down 5", "forward 1", "forward 9", "down 9", "forward 5", "down 1", "up 9", "down 3", "down 2", "up 6", "forward 4", "down 6", "down 2", "forward 5", "up 8", "down 7", "up 6", "down 1", "forward 3", "forward 4", "down 9", "down 2", "forward 5", "forward 8", "up 2", "forward 9", "up 5", "down 1", "forward 1", "forward 6", "forward 9", "forward 2", "down 8", "forward 1", "up 8", "forward 1", "forward 8", "up 5", "forward 3", "down 4", "up 8", "down 6", "up 1", "forward 2", "forward 4", "up 6", "forward 1", "forward 4", "up 6", "down 8", "up 7", "forward 5", "up 7", "down 9", "down 2", "up 5", "forward 2", "down 7", "forward 1", "forward 1", "forward 3", "down 6", "forward 7", "up 2", "up 1", "forward 1", "up 5", "down 1", "forward 7", "up 9", "forward 7", "down 6", "up 4", "down 5", "up 2", "down 4", "up 3", "down 6", "down 7", "down 7", "up 1", "forward 4", "forward 1", "up 2", "down 7", "down 5", "forward 7", "forward 5", "forward 2", "down 4", "down 6", "down 8", "forward 9", "forward 8", "up 9", "forward 6", "down 6", "down 6", "down 8", "up 5", "forward 6", "down 6", "forward 5", "down 4", "down 6", "down 2", "up 3", "forward 1", "forward 8", "down 2", "forward 9", "forward 7", "forward 8", "forward 1", "down 8", "forward 1", "down 1", "forward 6", "forward 4", "forward 5", "up 9", "up 1", "down 7", "up 8", "down 5", "down 2", "forward 9", "up 2", "forward 6", "down 7", "down 7", "down 2", "down 6", "down 7", "forward 9", "up 2", "down 6", "forward 8", "forward 1", "down 3", "down 2", "forward 3", "forward 7", "down 3", "down 1", "down 4", "down 9", "forward 1", "down 6", "down 8", "down 4", "down 5", "down 5", "up 9", "forward 7", "down 9", "up 3", "up 5", "up 1", "forward 2", "forward 9", "down 3", "forward 1", "up 3", "forward 6", "up 2", "forward 7", "forward 5", "forward 8", "forward 7", "forward 8", "down 5", "up 3", "forward 5", "forward 8", "forward 1", "forward 5", "forward 7", "forward 2", "down 6", "down 6", "down 6", "down 2", "down 2", "up 1", "down 1", "forward 5", "forward 7", "down 4", "forward 4", "up 1", "forward 7", "forward 8", "down 5", "down 2", "down 2", "down 5", "forward 3", "forward 7", "down 5", "forward 2", "forward 8", "down 3", "forward 6", "up 1", "forward 7", "forward 4", "forward 1", "forward 9", "forward 2", "down 1", "down 1", "down 8", "forward 4", "down 6", "forward 7", "forward 2"];
+
+console.log(`day2Input array has ${day2Input.length} elements`);
+
+// reducer function to sum all the elements of the forward, up and down arrays
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+// ------------------------------------
+// FORWARD ARRAY
+
+// Create array with forward entries only
+let horizontalArray = day2Input.filter(el => el.includes("forward"));
+
+// console.log(horizontalArray);
+// console.log(`horizontalArray has ${horizontalArray.length} elements`);
+
+// Remove "forward " text from each array element
+let horizontalArrayShort = horizontalArray.map(el => el.replace("forward ", ""));
+
+// console.log(horizontalArrayShort);
+// console.log(`horizontalArrayShort has ${horizontalArrayShort.length} elements`);
+
+// Converting the array from strings to numbers into a new array
+let horizontalArrayShortNum = horizontalArrayShort.map(num => parseInt(num, 10));
+
+// console.log(horizontalArrayShortNum);
+console.log(`horizontalArrayShortNum has ${horizontalArrayShort.length} elements`);
+
+// Adding all the numeric values from the horizontal array
+let totalHorizontalDistance = horizontalArrayShortNum.reduce(reducer);
+
+console.log(`The totalHorizontalDistance is ${totalHorizontalDistance} meters`);
+
+// ------------------------------------
+// DOWN ARRAY
+
+// Create array with down entries only
+let depthDownArray = day2Input.filter(el => el.includes("down"));
+
+// console.log(depthDownArray);
+// console.log(`depthDownArray has ${depthDownArray.length} elements`);
+
+// Remove "down " text from each array element
+let depthDownArrayShort = depthDownArray.map(el => el.replace("down ", ""));
+
+// console.log(depthDownArrayShort);
+// console.log(`depthDownArrayShort has ${depthDownArrayShort.length} elements`);
+
+// Converting the array from strings to numbers into a new array
+let depthDownArrayShortNum = depthDownArrayShort.map(num => parseInt(num, 10));
+
+// console.log(depthDownArrayShortNum);
+console.log(`depthDownArrayShortNum has ${depthDownArrayShort.length} elements`);
+
+// Adding all the numeric values from the horizontal array
+let totalDepthDownDistance = depthDownArrayShortNum.reduce(reducer);
+
+console.log(`The totalDepthDownDistance is ${totalDepthDownDistance} meters`);
+
+// ------------------------------------
+// UP ARRAY
+
+// Create array with up entries only
+let depthUpArray = day2Input.filter(el => el.includes("up"));
+
+// console.log(depthUpArray);
+// console.log(`depthUpArray has ${depthUpArray.length} elements`);
+
+// Remove "up " text from each array element
+let depthUpArrayShort = depthUpArray.map(el => el.replace("up ", ""));
+
+// console.log(depthUpArrayShort);
+// console.log(`depthUpArrayShort has ${depthUpArrayShort.length} elements`);
+
+// Converting the array from strings to numbers into a new array
+let depthUpArrayShortNum = depthUpArrayShort.map(num => parseInt(num, 10));
+
+// console.log(depthUpArrayShortNum);
+console.log(`depthUpArrayShortNum has ${depthUpArrayShort.length} elements`);
+
+// Adding all the numeric values from the horizontal array
+let totalDepthUpDistance = depthUpArrayShortNum.reduce(reducer);
+
+console.log(`The totalDepthUpDistance is ${totalDepthUpDistance} meters`);
+
+
+// ------------------------------------
+// Calculation of final depth
+let finalDepth = totalDepthDownDistance - totalDepthUpDistance;
+
+console.log(`The finalDepth is ${finalDepth} meters`);
+
+// Multiplication of last coordinates (horizontal x depth)
+
+let multiplicationOfLastCoordinates = totalHorizontalDistance * finalDepth;
+
+console.log(`The multiplicationOfLastCoordinates is ${multiplicationOfLastCoordinates}`);
+
+
+// ------------------------------------
+// console.log(`The 3 new arrays have ${horizontalArray.length+depthUpArray.length+depthDownArray.length} elements`)
+
